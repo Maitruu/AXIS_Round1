e1cd8d93a71d347975540e72e83d814917a99389
Saransh Dwivedi <saranshdwivedi08@gmail.com>
2024-03-15 21:07:44 +0530

    new

---
+++ axis_task/src/App.jsx
@@ -0,0 +1,11 @@
+import Desk from "./components/desk";
+
+function App() {
+  return (
+    <>
+      <Desk />
+    </>
+  );
+}
+
+export default App;
---
+++ axis_task/src/components/desk.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const desk = () => {
+  return (
+    <>
+      <h1>Director's Desk</h1>
+      <div className="bg-black flex">
+        <div className="bg-red-100"></div>
+        <div className="bg-slate-300"></div>
+      </div>
+    </>
+  );
+};
+
+export default desk;
