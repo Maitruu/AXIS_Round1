95e7d0cdce6375c271b2286dbd99a97f11abc097
Maitruu <tmaitreya2004@gmail.com>
2024-03-15 22:29:26 +0530

    navbar done

---
+++ axis_task/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import './index.css';
+import Navbar from './components/navbar';
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    </>
+  );
+};
+
+export default App;
