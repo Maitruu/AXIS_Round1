28ba5178cb55b88e45115cef7a1027f39535fd16
Maitruu <tmaitreya2004@gmail.com>
2024-03-16 16:53:25 +0530

    s

--- axis_task/src/components/ScrollToTopBottom.jsx
+++ axis_task/src/components/ScrollToTopBottom.jsx
@@ -5,8 +5,8 @@ import gif from '../assests/image.imageformat.text50percent.1562089386.gif';
 export default function ScrollToTopButton(){
     return(
         <div className="relative z-[300] ">
-            <ScrollToTop showUnder={50}>
-                <img src={gif} alt="gif" className="w-24 -rotate-90"/>
+            <ScrollToTop showUnder={40}>
+                <img src={gif} alt="gif" className="w-28 -rotate-90"/>
             </ScrollToTop>
         </div>
     )
