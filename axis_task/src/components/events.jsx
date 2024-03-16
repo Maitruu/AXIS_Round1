f93a85532ec405445028919c64a86b657101ad60
Maitruu <tmaitreya2004@gmail.com>
2024-03-16 16:14:27 +0530

    abc

--- axis_task/src/components/events.jsx
+++ axis_task/src/components/events.jsx
@@ -12,8 +12,8 @@ const events = () => {
           Events
         </h1>
       </div>
-      <div className="grid grid-cols-1 justify-items-center md:grid-cols-2 lg:grid-cols-4 p-5">
-        <div className="grid grids-cols-1 justify-items-center rounded-xl m-5 w-2/3 h-3/4 sm:w-3/4 bg-[url('https://www.niet.co.in/blog/wp-content/uploads/2020/09/robotics_Blog-1.jpg')] bg-cover backdrop-blur p-10 ">
+      <div className="grid grid-cols-1 justify-items-center md:grid-cols-2 lg:grid-cols-4 p-5 ">
+        <div className="grid grids-cols-1 justify-items-center rounded-xl m-5 w-2/3 h-1/2 sm:w-3/4 bg-[url('https://www.niet.co.in/blog/wp-content/uploads/2020/09/robotics_Blog-1.jpg')] bg-cover backdrop-blur p-10 ">
           <img
             className=" sm:w-1/2 rounded-full object-contain bg-white border-[#33415C] hover:w-2/3"
             src={robogif}
@@ -30,7 +30,7 @@ const events = () => {
           </div>
         </div>
 
-        <div className="grid grid-cols-1 justify-items-center rounded-xl w-2/3 h-3/4 sm:w-3/4 bg-[url('https://www.dnv.com/siteassets/images/dtc---data-analytics-and-data-science_1000x500.jpg?width=1288')] bg-cover backdrop-blur m-5 p-10">
+        <div className="grid grid-cols-1 justify-items-center rounded-xl w-2/3 h-1/2 sm:w-3/4 bg-[url('https://www.dnv.com/siteassets/images/dtc---data-analytics-and-data-science_1000x500.jpg?width=1288')] bg-cover backdrop-blur m-5 p-10">
           <img
             className="sm:w-1/2 rounded-full object-contain bg-white border-[#33415C] hover:w-2/3"
             src={datagif}
@@ -47,7 +47,7 @@ const events = () => {
           </div>
         </div>
 
-        <div className=" grid grid-cols-1 justify-items-center rounded-xl gap-2 w-2/3 h-3/4 sm:w-3/4 bg-[url('https://online.stanford.edu/sites/default/files/styles/embedded_large/public/2020-03/virtual-design-construction_CEE341.jpg?itok=8anoz1_t')] bg-cover backdrop-blur m-5 p-10">
+        <div className=" grid grid-cols-1 justify-items-center rounded-xl gap-2 w-2/3 h-1/2 sm:w-3/4 bg-[url('https://online.stanford.edu/sites/default/files/styles/embedded_large/public/2020-03/virtual-design-construction_CEE341.jpg?itok=8anoz1_t')] bg-cover backdrop-blur m-5 p-10">
           <img
             className=" sm:w-1/2 rounded-full object-contain bg-white border-[#33415C] hover:w-2/3"
             src={constructgif}
@@ -64,7 +64,7 @@ const events = () => {
           </div>
         </div>
 
-        <div className="grid grid-cols-1 justify-items-center rounded-xl gap-2 w-2/3 h-3/4 sm:w-3/4 bg-[url('https://5.imimg.com/data5/IJ/GK/MY-8776595/pcb-design-services-500x500.jpg')] bg-cover backdrop-blur m-5 p-10">
+        <div className="grid grid-cols-1 justify-items-center rounded-xl gap-2 w-2/3 h-1/2 sm:w-3/4 bg-[url('https://5.imimg.com/data5/IJ/GK/MY-8776595/pcb-design-services-500x500.jpg')] bg-cover backdrop-blur m-5 p-10">
           <img
             className="sm:w-1/2 rounded-full object-contain bg-white border-[#33415C] hover:w-2/3"
             src={software}
